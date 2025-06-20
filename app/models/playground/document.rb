2f1ffd4cabe8fc7d6e56f7c2bfce1e0a994c3d83
NicolasSNavarro <nicolassnavarro12@gmail.com>
2025-06-20 14:52:23 -0300

    fix: quit commas

--- app/models/playground/document.rb
+++ app/models/playground/document.rb
@@ -3,8 +3,8 @@ module Playground
     include Enumerable
 
     def initialize(title, author, content: "")
-      @title = title,
-      @author = author,
+      @title = title
+      @author = author
       @content = content
     end
 
